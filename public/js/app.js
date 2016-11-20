4f58cf73910248c74c54d30aed78a75bce21a378
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-20 12:53:36 -0800

    formatting

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,19 @@
 
 $(function() {
 
-console.log('javascript is working');
+	console.log('javascript is working');
 
-var mymap = L.map('mapid').setView([34.0195, -118.4912], 13);
+	var mymap = L.map('mapid').setView([34.0195, -118.4912], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
 
-  var marker = L.marker([34.0195, -118.4912]).addTo(mymap);
+	var marker = L.marker([34.0195, -118.4912]).addTo(mymap);
 
 	$('#submit').click(function(event) {
 
