40facbaea38041c4bcb78dfd1457f399a5f759c8
Antonio Rhee <antoniolrhee@gmail.com>
2016-11-20 19:39:41 -0800

    changes to style.css

--- public/js/group.js
+++ public/js/group.js
@@ -3,22 +3,22 @@ $(function() {
 // with prompt to get name once
 // var name = prompt('What is your name?')
 // var socket = io()
-// $('form').submit(function(){
+// $('form').submit(function() {
 // 	socket.emit('chat message', name + ': ' + $('#m').val())
 // 	$('#m').val('')
 // 	return false
 // })
-// socket.on('chat message', function(msg){
+// socket.on('chat message', function(msg) {
 // 	$('#messages').append($('<li>').text(msg))
 // })
 // with input for name
 var socket = io()
-$('form').submit(function(){
+$('form').submit(function() {
 	socket.emit('chat message', $('#name').val() + ': ' + $('#m').val());
 	$('#m').val('');
 	return false;
 });
-socket.on('chat message', function(msg){
+socket.on('chat message', function(msg) {
 	$('#messages').append($('<li>').text(msg));
 });
 
