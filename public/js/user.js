ec25df3fad72b9fa1085b5069fe8ad6ebb582df2
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-21 15:15:53 -0800

    starts data query with google map

--- public/js/user.js
+++ public/js/user.js
@@ -1,63 +1,70 @@
-// This page includes the javscript for the front-end
+var mongoId = "583337a4b416e70012a50a5d";
+var userLat = 34.0129821;
+var userLng = -118.495196;
+var searchResult = [];
 
-// $(function() {
+console.log('javascript is working');
 
-	var mongoId = "583337a4b416e70012a50a5d";
+function initMap() {
+      var uluru = {lat: userLat, lng: userLng};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    }
 
-	console.log('javascript is working');
+$('#submit').click(function(event) {
 
-	function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+  console.log('jQuery works')
+	var city = $('#city').val();
+	var state = $('#state').val();
+  var address = $('#address').val();
+  var zipcode = $('#zipcode').val();
+  var price = $('#price').val();
+  var bedrooms = $('#bedrooms').val();
+  var bathrooms = $('#bathrooms').val();
+  var duration = $('#duration').val();
+  var pets = $('#pets').val();
+  var furnished = $('#furnished').val();
 
-	$('#submit').click(function(event) {
+	var params = {
+		"city": city,
+		"state": state,
+		"address": address,
+		"zipcode": zipcode,
+		"price": price,
+		"bedrooms": bedrooms,
+		"bathrooms": bathrooms,
+		"duration": duration,
+		"pets": pets,
+		"furnished": furnished
+	 }
 
-    console.log('jQuery works')
-		var city = $('#city').val();
-		var state = $('#state').val();
-    var address = $('#address').val();
-    var zipcode = $('#zipcode').val();
-    var price = $('#price').val();
-    var bedrooms = $('#bedrooms').val();
-    var bathrooms = $('#bathrooms').val();
-    var duration = $('#duration').val();
-    var pets = $('#pets').val();
-    var furnished = $('#furnished').val();
+	 console.log(params);
 
-		var params = {
-			"city": city,
-			"state": state,
-			"address": address,
-			"zipcode": zipcode,
-			"price": price,
-			"bedrooms": bedrooms,
-			"bathrooms": bathrooms,
-			"duration": duration,
-			"pets": pets,
-			"furnished": furnished
-		 }
+  $.post( "http://homeme-api.herokuapp.com/listings", params, "json");
 
-		 console.log(params);
-
-    $.post( "http://homeme-api.herokuapp.com/listings", params, "json");
-
-    event.preventDefault();
-  });
-
-	console.log('foo');
-	$.get("http://homeme-api.herokuapp.com/listings", {"id": mongoId}, function(data) {
-		console.log(data.neighborhood);
-		console.log(data.latitude);
-		console.log(data.longitude);
-	}, "json");
-
-// end of document.ready function
-// });
+  event.preventDefault();
+});
+// {"id": mongoId}
+$.get("http://homeme-api.herokuapp.com/listings", {}, function(data) {
+	for (var i = 0; i < data.length; i++) {
+		if (
+			data[i].price <= highPrice &&
+			data[i].price >= lowPrice &&
+			data[i].bedrooms === bedrooms &&
+			data[i].bathrooms === bathrooms &&
+			data[i].pets === pets &&
+			data[i].furnished === furnished &&
+			) {
+			searchResult.push(data[i]);
+		}
+	}
+	console.log(searchResult);
+	// console.log(data.latitude);
+	// console.log(data.longitude);
+}, "json");
