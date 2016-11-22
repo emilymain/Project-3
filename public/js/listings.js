2df655ea5c4b7663ade1c42199baf016a2c6068d
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-21 23:42:19 -0800

    renders google map pins

--- public/js/listings.js
+++ public/js/listings.js
@@ -1,64 +1,66 @@
-// var mongoId = "583337a4b416e70012a50a5d";
-var userLat = 34.0129821;
-var userLng = -118.495196;
 var searchResult = [];
-
+// 34.0129821
+// -118.495196
 console.log('javascript is working');
 
 function initMap() {
-      var uluru = {lat: userLat, lng: userLng};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11,
-        center: uluru
-      });
-      var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
-    }
+	return new google.maps.Map($('#map')[0], {
+		zoom: 11,
+		center: { lat: 34.0129821, lng: -118.495196 }
+	})
+};
 
-    $('#submitSearch').click(function(event) {
+var map = initMap();
 
-      console.log('jQuery works')
-    	var city = $('#city').val();
-    	var minPrice = $('#minPrice').val();
-      var maxPrice = $('#maxPrice').val();
-      var bedrooms = $('#bedrooms').val();
-      var bathrooms = $('#bathrooms').val();
-      var duration = $('#duration').val();
-      var pets = $('#pets').val();
-      var furnished = $('#furnished').val();
+function populateMap(params) {
+	$.get("http://localhost:3000/api/listings", {}, function (data) {
+		for (var i = 0; i < data.length; i++) {
+			if (
+				data[i].price <= params.maxPrice &&
+				data[i].price >= params.minPrice
+				// &&
+				// data[i].bedrooms === params.bedrooms &&
+				// data[i].bathrooms === params.bathrooms &&
+				// data[i].pets === params.pets &&
+				// data[i].furnished === params.furnished &&
+			) {
+				var uluru = {
+					lat: data[i].latitude,
+					lng: data[i].longitude
+				};
 
-    	var params = {
-    		"city": city,
-    		"minPrice": minPrice,
-    		"maxPrice": maxPrice,
-    		"bedrooms": bedrooms,
-    		"bathrooms": bathrooms,
-    		"duration": duration,
-    		"pets": pets,
-    		"furnished": furnished
-    	 }
+				var marker = new google.maps.Marker({
+					position: uluru,
+					map: map
+				});
 
+				searchResult.push(data[i]);
+			};
+		}
+		console.log(searchResult);
+	}, "json");
+  searchResult = [];
+};
 
-      $.post( "http://localhost:3000/api/listings", params, "json");
-
-      event.preventDefault();
-
-      $.get("http://localhost:3000/api/listings", {}, function(data) {
-      	for (var i = 0; i < data.length; i++) {
-      		if (
-      			data[i].price <= params.maxPrice &&
-      			data[i].price >= params.minPrice
-            // &&
-      			// data[i].bedrooms === params.bedrooms &&
-      			// data[i].bathrooms === params.bathrooms &&
-      			// data[i].pets === params.pets &&
-      			// data[i].furnished === params.furnished &&
-      			) {
-      			searchResult.push(data[i]);
-      		}
-      	}
-      	console.log(searchResult);
-      }, "json");
-    });
+$('#submitSearch').click(function(event) {
+  console.log('jQuery works')
+	var city = $('#city').val();
+	var minPrice = $('#minPrice').val();
+  var maxPrice = $('#maxPrice').val();
+  var bedrooms = $('#bedrooms').val();
+  var bathrooms = $('#bathrooms').val();
+  var duration = $('#duration').val();
+  var pets = $('#pets').val();
+  var furnished = $('#furnished').val();
+	var params = {
+		"city": city,
+		"minPrice": minPrice,
+		"maxPrice": maxPrice,
+		"bedrooms": bedrooms,
+		"bathrooms": bathrooms,
+		"duration": duration,
+		"pets": pets,
+		"furnished": furnished
+	}
+  populateMap(params);
+});
