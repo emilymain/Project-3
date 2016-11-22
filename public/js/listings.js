a0ba9356d014466ea2eba3df21ddcfbf7965e32f
Emily Main <emilymuirmain@gmail.com>
2016-11-22 14:07:11 -0800

    added image upload to new listings

--- public/js/listings.js
+++ public/js/listings.js
@@ -1,69 +1,100 @@
+var map;
+var markers = [];
 var searchResult = [];
-// 34.0129821
-// -118.495196
-console.log('javascript is working');
-var uluru;
 
 function initMap() {
-	return new google.maps.Map($('#map')[0], {
-		zoom: 11,
-		center: { lat: 34.0129821, lng: -118.495196 }
-	})
-};
+  var center = {
+    lat: 34.0129821,
+    lng: -118.495196
+  };
 
-var map = initMap();
+  map = new google.maps.Map($('#map')[0], {
+    zoom: 12,
+    center: center,
+    mapTypeId: 'terrain'
+  });
 
-function populateMap(params) {
-	$.get("http://localhost:3000/api/listings", {}, function (data) {
-		for (var i = 0; i < data.length; i++) {
-			if (
-        data[i].city === params.city
-				// || data[i].price <= params.minPrice
-				// || data[i].price >= params.maxPrice
-				// || data[i].bedrooms === params.bedrooms
-				// || data[i].bathrooms === params.bathrooms
-				// || data[i].duration === params.duration
-				// || data[i].pets === params.pets
-				// || data[i].furnished === params.furnished
-			) {
-				var uluru = {
-					lat: data[i].latitude,
-					lng: data[i].longitude
-				};
+  // This event listener will call addMarker() when the map is clicked.
+  $('#submitSearch').click(function(event) {
+    deleteMarkers();
+    searchResult = [];
 
-				var marker = new google.maps.Marker({
-					position: uluru,
-					map: map
-				});
+    var city = $('#city').val();
+    var minPrice = $('#minPrice').val();
+    var maxPrice = $('#maxPrice').val();
+    var bedrooms = $('#bedrooms').val();
+    var bathrooms = $('#bathrooms').val();
+    var duration = $('#duration').val();
+    var pets = $('#pets').val();
+    var furnished = $('#furnished').val();
 
-				searchResult.push(data[i]);
-			};
-		}
-		console.log(searchResult);
-	}, "json");
-  searchResult = [];
- };
+    var params = {
+      "city": city,
+      "minPrice": minPrice,
+      "maxPrice": maxPrice,
+      "bedrooms": bedrooms,
+      "bathrooms": bathrooms,
+      "duration": duration,
+      "pets": pets,
+      "furnished": furnished
+    }
 
-$('#submitSearch').click(function(event) {
-  console.log('jQuery works')
+    $.get("http://localhost:3000/api/listings", {}, function(data) {
+      for (var i = 0; i < data.length; i++) {
+        if (
+          data[i].city === params.city
+          // || data[i].price <= params.minPrice
+          // || data[i].price >= params.maxPrice
+          // || data[i].bedrooms === params.bedrooms
+          // || data[i].bathrooms === params.bathrooms
+          // || data[i].duration === params.duration
+          // || data[i].pets === params.pets
+          // || data[i].furnished === params.furnished
+        ) {
+          searchResult.push(data[i])
+          var latLng = {
+            lat: data[i].latitude,
+            lng: data[i].longitude
+          }
+          addMarker(latLng);
+        }
+      }
+      console.log(searchResult);
+    })
+  });
 
-	var city = $('#city').val();
-	var minPrice = $('#minPrice').val();
-  var maxPrice = $('#maxPrice').val();
-  var bedrooms = $('#bedrooms').val();
-  var bathrooms = $('#bathrooms').val();
-  var duration = $('#duration').val();
-  var pets = $('#pets').val();
-  var furnished = $('#furnished').val();
-	var params = {
-		"city": city,
-		"minPrice": minPrice,
-		"maxPrice": maxPrice,
-		"bedrooms": bedrooms,
-		"bathrooms": bathrooms,
-		"duration": duration,
-		"pets": pets,
-		"furnished": furnished
-	}
-  populateMap(params);
-});
+  // Adds a marker at the center of the map.
+  addMarker(center);
+}
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
