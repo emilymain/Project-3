f5bb4138a372f16849091b3be34d9b269a6b958b
Emily Main <emilymuirmain@gmail.com>
2016-11-23 16:01:59 -0800

    worked on newlistings page

--- controllers/mylistings.js
+++ controllers/mylistings.js
@@ -1,9 +1,30 @@
-// var Listing = require('../models/listing');
+var Listing = require('../models/listing');
+var User = require('../models/user');
 
 module.exports = {
   index: index
 }
 
 function index(req, res, next) {
-  res.render('mylistings.ejs', { title: 'HomeMe', user: req.user });
+  console.log(req.user);
+  // User.find(req.user._id, function(err, user){
+  //   if (err) throw err
+  //   Listing.find({}, function(err, listings) {
+  //     if(err) throw err;
+  //     var list = listings[0]
+  //     User.findOneAndUpdate(user, {$push: {'favorites': list}}, {upsert: true}, function(err, user){
+  //       console.log(user)
+  //     })
+  //   });
+  // })
+  var user = req.user;
+  var faves = [];
+  user.favorites.forEach(function(list) {
+    Listing.findById(list._id, function(err, listing) {
+      if(err) throw err;
+
+      faves.push(listing);
+    });
+  });
+  res.render('mylistings.ejs', { title: 'HomeMe', user: req.user, faves: faves });
 }
--- public/js/mylistings.js
+++ public/js/mylistings.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
-$.get("http://localhost:3000/api/listings", {}, function(data) {
-  console.log(data[0]);
+// $.get("http://localhost:3000/api/listings", {}, function(data) {
+//   $('h2').append(data[0].bedrooms)
+//   console.log(data[0]);
+//
+//
+// });
 
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// $(function() {
+//   $.ajax({
+//     type: "GET",
+//     url: "/api/listings"
+//   }).then(
+//     function(data)
+//
+//   )
 });
