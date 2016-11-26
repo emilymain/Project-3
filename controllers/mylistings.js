52edf8085ed7c139744c4f659010c08036068d50
Antonio Rhee <antoniolrhee@gmail.com>
2016-11-25 16:08:27 -0800

    adds stuff for favorite listings

--- controllers/mylistings.js
+++ controllers/mylistings.js
@@ -7,7 +7,7 @@ module.exports = {
 }
 
 function index(req, res, next) {
-  res.render('mylistings.ejs', { title: 'HomeMe', user: req.user, faves: faves });
+  res.render('mylistings.ejs', { title: 'HomeMe', user: req.user });
 }
 
 function addFaves(req, res, next) {
@@ -22,5 +22,5 @@ function addFaves(req, res, next) {
       if(err) throw err;
 
       res.json(user);
-    })
+    });
 }
--- public/js/mylistings.js
+++ public/js/mylistings.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 $.get("http://localhost:3000/api/listings", {}, function(data) {
-  $('h2').append(data[0].bedrooms)
-  console.log(data[0]);
+  for(var i = 0; i < data.length; i++) {
+    console.log(data[i]);
+  }
+  // 
+  // $('h3').text(data[0].streetNumber + ' ' + data[0].streetName)
+  // console.log(data[0]);
 
+// the above code uses /api/listings in order to grab data and append it to the div
+// the goal is to pull from the favorites array and do a forEach favorite, create a div for apt.
 
 });
 
