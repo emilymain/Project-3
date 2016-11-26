f6a8a62e6a0974eb3860937a9f7be8c99382396f
Antonio Rhee <antoniolrhee@gmail.com>
2016-11-25 17:54:28 -0800

    organizes divs for each apt listing

--- public/js/mylistings.js
+++ public/js/mylistings.js
@@ -1,35 +1,19 @@
 $(document).ready(function(){
-$.get("http://localhost:3000/api/listings", {}, function(data) {
-  for(var i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    $(`<div class="row">
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail"><img src="${data[i].imageurl}" alt="...">
-            <div class="caption">
-              <h3 id="address">${data[i].streetNumber + ' ' + data[i].streetName}</h3>
-              <p>${data[i].neighborhood + ', ' + data[i].zipcode}</p>
-              <p>${data[i].bedrooms + ' Bed, ' + data[i].bathrooms + ' Bath'}</p>
-              <p>${'$' + data[i].price + '/mo'} </p>
+  $.get("http://localhost:3000/api/listings", {}, function(data) {
+    for(var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      $(`<div class="row">
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail"><img src="${data[i].imageurl}" alt="...">
+              <div class="caption">
+                <h3 id="address">${data[i].streetNumber + ' ' + data[i].streetName}</h3>
+                <p>${data[i].neighborhood + ', ' + data[i].zipcode}</p>
+                <p>${data[i].bedrooms + ' Bed, ' + data[i].bathrooms + ' Bath'}</p>
+                <p>${'$' + data[i].price + '/mo'} </p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>`).appendTo('body');
-  }
-  //
-  // $('h3').text(data[0].streetNumber + ' ' + data[0].streetName)
-  // console.log(data[0]);
-
-// the above code uses /api/listings in order to grab data and append it to the div
-// the goal is to pull from the favorites array and do a forEach favorite, create a div for apt.
-
-});
-
-// $(function() {
-//   $.ajax({
-//     type: "GET",
-//     url: "/api/listings"
-//   }).then(
-//     function(data)
-//
-//   )
+        </div>`).appendTo('body');
+    }
+  });
 });
